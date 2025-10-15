3866bbbb58d33dd492b9349aae2af75273d03fb2
Remmie <chinemeremokolie12@gmail.com>
2025-10-15 19:12:00 +0100

    (Debugged the Program)

---
+++ week-3/practice_10.rs
@@ -0,0 +1,21 @@
+fn main() {
+	//addition
+	let sum:f32 = 5_550.0 + 7_310.0;
+	println!("The sum of 5550 and 7310 = {}",sum );
+
+	//subtraction
+	let difference:f32 = 95.5 - 4.3;
+	println!("The difference of 95.5 and 4.3 = {}",difference);
+
+	//multiplication
+	let product:f32 = 4.0 * 30.0;
+	println!("The multiple of 4 and 30 = {}", product);
+
+	//division
+	let quotient:f32 = 56.7 / 32.2;
+	println!("The division of 56.7 and 32.2 = {}",quotient );
+
+	//remainder
+	let remainder:f32 = 43.0 % 5.0;
+	println!("The remainder of 43 and 5 = {}",remainder );
+}
