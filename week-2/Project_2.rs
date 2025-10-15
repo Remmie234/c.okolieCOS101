c30b489f57bbeb03c396bdbbabc843a59392cbc9
Remmie <chinemeremokolie12@gmail.com>
2025-10-15 19:41:25 +0100

    (My week2 projects commit)

--- week-2/Project_2.rs
+++ week-2/Project_2.rs
@@ -4,13 +4,13 @@ fn main() {
 	let hp:f64 = 750_000.00;
 	let dell:f64 = 2_850_000.00;
 	let acer:f64 = 250_000.00;
-	let qty:f64 = 2.0 + 1.0 + 3.0 + 3.0 + 1.0;
+	let qty:f64 = 2.00 + 1.00 + 3.00 + 3.00 + 1.00;
 
-	//Calculate Sum
-	let sum:f64 = (2.0 * toshibo) + (1.0 * mac) + (3.0 * hp) + (3.0 * dell) + (1.0 * acer);
+	//Calculating Sum
+	let sum:f64 = (2.00 * toshibo) + (1.00 * mac) + (3.00 * hp) + (3.00 * dell) + (1.00 * acer);
 	println!("The sum of the following sales record is N{}", sum);
 
-	//Calculate Average
+	//Calculating Average
 	let avg:f64 = sum / qty;
 	println!("The Average of the following sales record is {}", avg);
 	
