5ee94f15a18da2ef534706aa6b9235a44d61efa0
Remmie <chinemeremokolie12@gmail.com>
2025-10-16 14:49:17 +0100

    (Debugged the program)

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-3\practice_10\src\main.rs
