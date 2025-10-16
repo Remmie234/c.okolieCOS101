5cae0a0f063392f0e44952174a0630069983cdcb
Remmie <chinemeremokolie12@gmail.com>
2025-10-16 14:28:14 +0100

    (Types of Variables)

--- week-3/practice_1/src/main.rs
+++ week-3/practice_1/src/main.rs
@@ -1,21 +1,12 @@
 fn main() {
-    //addition
-    let sum:f32 = 5_550.0 + 7_310.0;
-    println!("The sum of 5550 and 7310 = {}",sum );
+    let school_string = "School of Science and Technology";  //string type
+    let rating_float:f32 = 5.0;   //float type
+    let is_growing_boolean:bool = true;  //boolean type
+    let department_integer:i32 = 3;  //integer type
 
-    //subtraction
-    let difference:f32 = 95.5 - 4.3;
-    println!("The difference of 95.5 and 4.3 = {}",difference);
 
-    //multiplication
-    let product:f32 = 4.0 * 30.0;
-    println!("The multiple of 4 and 30 = {}", product);
-
-    //division
-    let quotient:f32 = 56.7 / 32.2;
-    println!("The division of 56.7 and 32.2 = {}",quotient );
-
-    //remainder
-    let remainder:f32 = 43.0 % 5.0;
-    println!("The remainder of 43 and 5 = {}",remainder );
+    println!("School Name is: {}",school_string);
+    println!("School rating on 5 is: {}", rating_float);
+    println!("School is growing : {}",is_growing_boolean );
+    println!("Number of Departments : {}",department_integer);
 }
