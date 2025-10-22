c73e9201608ee17cd1862da3dc85618569bbd739
Remmie <chinemeremokolie12@gmail.com>
2025-10-22 16:41:14 +0100

    (Standard Input Illustration)

---
+++ week-4/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_2\target\debug\practice_2.exe: C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_2\src\main.rs
