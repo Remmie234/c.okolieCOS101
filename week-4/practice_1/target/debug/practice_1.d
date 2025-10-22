6cfb130f209767a52ee9eb67009971ad614914a7
Remmie <chinemeremokolie12@gmail.com>
2025-10-22 16:13:33 +0100

    (Standard Input)

---
+++ week-4/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_1\target\debug\practice_1.exe: C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_1\src\main.rs
