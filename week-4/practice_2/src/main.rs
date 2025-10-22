7917f3f0ead912bf4666fb4bc9de041819d01dc0
Remmie <chinemeremokolie12@gmail.com>
2025-10-22 17:33:36 +0100

    (Standard Input Illustration)

--- week-4/practice_2/src/main.rs
+++ week-4/practice_2/src/main.rs
@@ -1,23 +1,27 @@
-// Rust program to calculate the area of a
-// triangle for a given base and height
-
-use std::io;
+// Rust program to calculate the area of a triangle given three sides
+use std :: io;
 
 fn main()
 {
-    let mut input1 = String::new();
-    let mut input2 = String::new();
+let mut input1 = String :: new();
+let mut input2 = String :: new();
+let mut input3 = String :: new();
 
-    println!("Enter base: ");
-    io::stdin().read_line(&mut input1).expect("Not a valid string");
-    let base:f32 = input1.trim().parse().expect("Not a valid number");
+println!("Enter first edge of triangle: ");
+io :: stdin().read_line(&mut input1).expect("Not a valid string");
+let a:f32 = input1.trim().parse().expect("Not a valid number");
 
-    println!("Enter height: ");
-    io::stdin().read_line(&mut input2).expect("Not a valid string");
-    let height:f32 = input2.trim().parse().expect("Not a valid number");
+println!("Enter second edge of triangle: ");
+io :: stdin().read_line(&mut input2).expect("Not a valid string");
+let b:f32 = input2.trim().parse().expect("Not a valid number");
 
-    if base > 0.00 {
-        let area:f32 = (base * height) / 2.00;
-        println!("Area of a triangle: {}", area);
-    }
+println!("Enter third edge of triangle: ");
+io :: stdin().read_line(&mut input3).expect("Not a valid string");
+let c:f32 = input3.trim().parse().expect("Not a valid number");
+
+let s:f32 = (a + b + c) / 2.0;
+let mut area:f32 = s * (s - a) * (s - b) * (s -c);
+area = area.sqrt();
+
+println!("Area of a triangle: {}", area);
 }
