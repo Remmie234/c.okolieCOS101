21e455528cde28ca4c3d9a866b82620b87ae6cc9
Remmie <chinemeremokolie12@gmail.com>
2025-10-22 18:20:04 +0100

    (Nested If Statement)

---
+++ week-4/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_5\target\debug\practice_5.exe: C:\Users\Administrator\OneDrive\Documents\c.okolieCOS101\week-4\practice_5\src\main.rs
