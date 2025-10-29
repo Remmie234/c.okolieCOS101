4fb1d0f420827bbaff3580ea67d775aa6e70d5a5
Remmie <chinemeremokolie12@gmail.com>
2025-10-29 11:21:47 +0100

    (Determining annual incentive)

--- week-4/project_2/src/main.rs
+++ week-4/project_2/src/main.rs
@@ -7,6 +7,7 @@ fn main()
     let mut input_name = String::new();
     let mut input_experience = String::new();
     let mut input_age = String::new();
+    let yes:bool = true;
 
 
     // input name
@@ -15,24 +16,24 @@ fn main()
     let name = input_name.trim();
 
     //input experience
-    println!("Employee {}, are you experienced (yes / no): ", name);
+    println!("Employee {}, you are experienced (true / false): ", name);
     io::stdin().read_line(&mut input_experience).expect("Failed to read input");
-    let experience = input_experience.trim().to_lowercase();
+    let experience:bool = input_experience.trim().parse().expect("Failed to read input");
 
     //input age
     println!("Employee {}, please enter your age (in integer): ", name);
     io::stdin().read_line(&mut input_age).expect("Failed to read input");
     let age:u32 = input_age.trim().parse().expect("Please enter a valid input");
 
-    if experience == "yes" && age >= 40
+    if experience == yes && age >= 40
     {
         println!("Employee {}, your incentive is N1,560,000.00", name);
     }
-    else if experience == "yes" && age >= 30 && age < 40
+    else if experience == yes && age >= 30 && age < 40
     {
     println!("Employee {}, your incentive should be N1,480,000.00", name);
     }
-    else if experience == "yes" && age < 28
+    else if experience == yes && age < 28
     {
     println!("Employee {}, your incentive should be N1,300,000.00", name);
     }
