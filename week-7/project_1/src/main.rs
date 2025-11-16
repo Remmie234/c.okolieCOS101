d736346d65f50dd6c82048e987a6c947a8c20b8f
Remmie <chinemeremokolie12@gmail.com>
2025-11-16 16:50:42 +0100

    Calculating the Area and Volume Of Different Shapes

---
+++ week-7/project_1/src/main.rs
@@ -0,0 +1,173 @@
+//Calculating the area or volume of different shapes🧮➕
+use std::io;
+
+//Equation Functions
+fn area_trapezium_formula()->f64 {
+    let mut height = String::new();
+    let mut base_1 = String::new();
+    let mut base_2 = String::new();
+
+    //Input
+    println!("Please input the height: ");
+    io::stdin().read_line(&mut height).expect("Failed to read input😞");
+    let h:f64 = height.trim().parse().unwrap_or(0.0);
+
+    println!("Please input the first base: ");
+    io::stdin().read_line(&mut base_1).expect("Failed to read input😞");
+    let b1:f64 = base_1.trim().parse().unwrap_or(0.0);
+
+    println!("Please input the second base: ");
+    io::stdin().read_line(&mut base_2).expect("Failed to read input😞");
+    let b2:f64 = base_2.trim().parse().unwrap_or(0.0); 
+
+    //Checking Validity
+    if h <= 0.0 || b1 <= 0.0 || b2 <= 0.0{
+        println!("Invalid value entered!😓 Therefore there is no area. And as such..");
+        return 0.0;
+    }
+
+    //Calculating Area
+    let area:f64 = (h / 2.0) * (b1 + b2);
+    return area;
+}
+
+fn area_rhombus_formula()->f64 {
+    let mut diagonal_1 = String::new();
+    let mut diagonal_2 = String::new();
+
+    //Inputs
+    println!("Please input the first diagonal: ");
+    io::stdin().read_line(&mut diagonal_1).expect("Failed to read input😞");
+    let d1:f64 = diagonal_1.trim().parse().unwrap_or(0.0);
+
+    println!("Please input the second diagonal: ");
+    io::stdin().read_line(&mut diagonal_2).expect("Failed to read input😞");
+    let d2:f64 = diagonal_2.trim().parse().unwrap_or(0.0);
+
+    //Checking Validity
+    if d1 <= 0.0 || d2 <= 0.0 {
+        println!("Invalid value entered!😓 Therefore there is no area. And as such..");
+        return 0.0;
+    }
+
+    //Calculating Area
+    let area:f64 = 0.5 * d1 * d2;
+    return area;
+}
+
+fn area_parallelogram_formula()->f64 {
+    let mut base = String::new();
+    let mut altitude = String::new();
+
+    //Inputs
+    println!("Please input the base: ");
+    io::stdin().read_line(&mut base).expect("Failed to read input😞");
+    let b:f64 = base.trim().parse().unwrap_or(0.0);
+
+    println!("Please input the altitude: ");
+    io::stdin().read_line(&mut altitude).expect("Failed to read input😞");
+    let a:f64 = altitude.trim().parse().unwrap_or(0.0);
+
+    //Checking Validity
+    if b <= 0.0 || a <= 0.0 {
+        println!("Invalid value entered!😓 Therefore there is no area. And as such..");
+        return 0.0;
+    }
+
+    //Calculating Area
+    let area:f64 = b * a;
+    return area;
+}
+
+fn area_cube_formula()->f64 {
+    let mut length = String::new();
+
+    //Input
+    println!("Please input the length of one side: ");
+    io::stdin().read_line(&mut length).expect("Failed to read input😞");
+    let l:f64 = length.trim().parse().unwrap_or(0.0);
+
+    //Checking Validity
+    if l <= 0.0 {
+        println!("Invalid value entered!😓 Therefore there is no area. And as such..");
+        return 0.0;
+    }
+
+    //Calculating Area
+    let area:f64 = 6.0 * l * l;
+    return area;
+}
+
+fn pi() ->f64 {
+    22.0 / 7.0
+}
+
+fn volume_cylinder_formula()->f64 {
+    let mut radius = String::new();
+    let mut height = String::new();
+
+    //Inputs
+    println!("Please input the radius: ");
+    io::stdin().read_line(&mut radius).expect("Failed to read input😞");
+    let r:f64 = radius.trim().parse().unwrap_or(0.0);
+
+    println!("Please input the height: ");
+    io::stdin().read_line(&mut height).expect("Failed to read input😞");
+    let he:f64 = height.trim().parse().unwrap_or(0.0);
+
+    //Checking Validity
+    if r <= 0.0 || he <= 0.0 {
+        println!("Invalid value entered!😓 Therefore there is no volume. And as such..");
+        return 0.0;
+    }
+
+    //Calculating Volume
+    let volume:f64 = pi() * r * r * he;
+    return volume;
+}
+
+//Main Function
+fn main() {
+    //Greetings
+    let mut name = String::new();
+    println!("Welcome! I greet you, Honored One!");
+    println!("Dear Honored One, please enter your name: ");
+    io::stdin().read_line(&mut name).expect("Failed to read input😞");
+    let customer = name.trim();
+
+    //Greetings with name
+    println!("Once again, I welcome you, {}", customer);
+
+    //Equations
+    println!("Here are the available list of Equations: 🧮➕\n
+            A = Area of Trapezium
+            B = Area of Rhombus
+            C = Area of Parallelogram
+            D = Area of Cube
+            E = Volume of Cylinder\n");
+
+    //Inputs
+    let mut input = String::new();
+    println!("Dear {}, please enter the equation code you would like to use (A, B, C, D, E): ", customer);
+    io::stdin().read_line(&mut input).expect("Failed to read input😞");
+
+    // Converting input to uppercase for consistency
+    let equation_code = input.trim().to_uppercase();
+
+    //Invoking Equation function
+    if equation_code == "A" {
+        println!("Dear {}, the area of the Trapezium is {} ✌🏼😁", customer, area_trapezium_formula());
+    } else if equation_code == "B" {
+        println!("Dear {}, the area of the Rhombus is {} ✌🏼😁", customer,area_rhombus_formula());
+    } else if equation_code == "C" {
+        println!("Dear {}, the area of the Parallelogram is {} ✌🏼😁", customer,area_parallelogram_formula());
+    } else if equation_code == "D" {
+        println!("Dear {}, the area of the cube is {} ✌🏼😁", customer,area_cube_formula());
+    } else if equation_code == "E" {
+        println!("Dear {}, the volume of the Cylinder is {} ✌🏼😁", customer,volume_cylinder_formula());
+    } else {
+        println!("Invalid code entered!😓");
+        return
+    }
+
+}
---
+++ week-7/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-7\project_1\target\debug\deps\project_1.d: src\main.rs
+
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-7\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+src\main.rs:
