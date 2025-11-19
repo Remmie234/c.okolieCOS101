f19d496b3f0cbced21a1a31030a99e82eb13961c
Remmie <chinemeremokolie12@gmail.com>
2025-11-19 17:04:41 +0100

    Adding two vectors

---
+++ week-8/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-8\practice_6\target\debug\deps\practice_6.d: src\main.rs
+
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-8\practice_6\target\debug\deps\practice_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Downloads\PAU\Introduction\ to\ Computer\ Science\ COS\ 101\c.okolieCOS101\week-8\practice_6\target\debug\practice_6.exe: C:\Users\Administrator\Downloads\PAU\Introduction\ to\ Computer\ Science\ COS\ 101\c.okolieCOS101\week-8\practice_6\src\main.rs
