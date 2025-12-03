13b981afc2852006ede2fbf1983420aba2dc946e
Remmie <chinemeremokolie12@gmail.com>
2025-12-03 17:07:40 +0100

    Passing value to a function.

---
+++ week-10/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-10\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\Administrator\Downloads\PAU\Introduction to Computer Science COS 101\c.okolieCOS101\week-10\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
